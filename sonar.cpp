1b6f55d4307419dd23987b31f1eb8e5ee8ab2c99
SpeedyGo55 <speedygo55@outlook.com>
2025-04-04 20:56:24 +0200

    setup

---
+++ sonar.cpp
@@ -0,0 +1,17 @@
+/**
+* Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main() {
+    stdio_init_all();
+    while (true)
+    {
+        printf("Hello serial!\n");
+        sleep_ms(2000);
+    }
+
+}
