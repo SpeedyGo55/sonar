75b30df0c93f20ad0daf13a920bf01ce5f819e44
SpeedyGo55 <speedygo55@outlook.com>
2025-04-06 14:01:00 +0200

    added servo functionality

--- sonar.cpp
+++ sonar.cpp
@@ -3,15 +3,39 @@
 *
 * SPDX-License-Identifier: BSD-3-Clause
 */
-#include "pico/stdlib.h"
+#include <pico/stdlib.h>
 #include <stdio.h>
+#include <string>
+extern "C" {
+    #include "pico_servo.h"
+}
+
+#define MAX_PULSE 2500
+#define MIN_PULSE 500
+#define SERVO_PIN 3
+
 
 int main() {
+    servo_init();
+    servo_clock_auto();
+    servo_attach(SERVO_PIN);
+    servo_set_bounds(300, 1250);
     stdio_init_all();
+    int angle = 100;
+    bool direction = true;
     while (true)
     {
-        printf("Hello serial!\n");
-        sleep_ms(2000);
+        servo_move_to(SERVO_PIN, angle);
+        printf("%d\n", angle);
+        sleep_ms(10);
+        if (angle >= 180 || angle <= 0) {
+            direction = !direction;
+        }
+        if (direction) {
+            angle++;
+        } else {
+            angle--;
+        }
     }
 
 }
